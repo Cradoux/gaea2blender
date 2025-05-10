043d9eb42046a63d06036c46649f6d8b8f80620e
Cradoux <cradoux.cr@gmail.com>
2025-05-10 10:53:21 +0100

    Fix ui and operators packages exposure for addon load

--- kilroys_gaea2blender/operators/__init__.py
+++ kilroys_gaea2blender/operators/__init__.py
@@ -1,3 +1,13 @@
+from importlib import import_module
+
 classes = []
+
+for mod_name in ('.globe', '.landscape'):
+    try:
+        m = import_module(mod_name, package=__name__)
+        if hasattr(m, 'classes'):
+            classes.extend(m.classes)
+    except ModuleNotFoundError:
+        continue
 
 # sub-modules will append to this list when they are imported 
--- kilroys_gaea2blender/ui/__init__.py
+++ kilroys_gaea2blender/ui/__init__.py
@@ -1,1 +1,6 @@
-classes = [] 
+from importlib import import_module
+
+panel_module = import_module('.panel', package=__name__)
+MainPanel = panel_module.MainPanel
+
+classes = [MainPanel] 
